6e172fd57e305bdd378dd606a62d3fa0a378062e
mazayastudio <mazaya.studio@outlook.com>
2020-08-01 10:25:29 +0700

    Pause the materi until Update Event -- have no idea what's going on --

--- calendar_app/src/app/components/CalendarEvent.vue
+++ calendar_app/src/app/components/CalendarEvent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="day-event" :style="getEventBackgroundColor">
-    <div>
+    <div v-if="!event.edit">
       <span class="has-text-centered details">{{ event.details }}</span>
       <div class="has-text-centered icons">
         <i
@@ -11,23 +11,46 @@
       </div>
     </div>
     <div v-if="event.edit">
-      <input type="text" :placeholder="event.details" />
+      <input
+        type="text"
+        :placeholder="event.details"
+        v-model="newEventDetails"
+      />
       <div class="has-text-centered icons">
-        <i class="fa fa-check"></i>
+        <i
+          class="fa fa-check"
+          @click="updateEvent(day.id, event.details, newEventDetails)"
+        ></i>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { store } from "../store";
 export default {
   name: "CalendarEvent",
   props: ["event", "day"],
+  data() {
+    return {
+      newEventDetails: "",
+    };
+  },
   computed: {
     getEventBackgroundColor() {
       const colors = ["#FF9999", "#85D6FF", "#99FF99"];
       let randomColor = colors[Math.floor(Math.random() * colors.length)];
       return `background-color: ${randomColor}`;
+    },
+  },
+  methods: {
+    editEvent(dayId, eventDetails) {
+      store.editEvent(dayId, eventDetails);
+    },
+    updateEvent(dayId, originalEventDetails, updatedEventDetails) {
+      if (updatedEventDetails === "")
+        updatedEventDetails = originalEventDetails;
+      store.updateEvent(dayId, originalEventDetails, updatedEventDetails);
     },
   },
 };
--- calendar_app/src/app/store.js
+++ calendar_app/src/app/store.js
@@ -16,4 +16,31 @@ export const store = {
     const activeDay = this.getActiveDay();
     activeDay.events.push({ details: eventDetails, edit: false });
   },
+  editEvent(dayId, eventDetails) {
+    this.resetEditOfAllEvents();
+    const dayObj = this.state.seedData.find((day) => day.id === dayId);
+    const eventObj = dayObj.events.find(
+      (event) => event.details === eventDetails
+    );
+    eventObj.edit = true;
+  },
+  resetEditOfAllEvents() {
+    this.state.seedData.map((dayObj) => {
+      dayObj.events.map((event) => {
+        event.edit = false;
+      });
+    });
+  },
+  updateEvent(dayId, originalEventDetails, newEventDetails) {
+    // Find the day object
+    const dayObj = this.state.seedData.find((day) => day.id === dayId);
+    // Find the specific event
+    const evenObj = dayObj.events.find(
+      (event) => event.details === originalEventDetails
+    );
+    // Set the event details to new details
+    // and turn off editing
+    eventObj.details = new EventDetails();
+    eventObj.edit = false;
+  },
 };
